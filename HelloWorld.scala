b86b944c4a085ad47a47e3123353e114bd284c73
nrhawkins <nrhawkins@gmail.com>
2015-03-15 19:10:33 -0700

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,21 @@
+package experimenting
+
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations
+import edu.stanford.nlp.dcoref.SieveCoreferenceSystem
+import edu.stanford.nlp.dcoref.Document
+import edu.stanford.nlp.dcoref.RuleBasedCorefMentionFinder
+import edu.stanford.nlp.dcoref.Dictionaries
+
+object HelloWorld {
+
+ def main(Args: Array[String]){
+ 
+   println("Hello World!")
+ 
+ }
+
+}
+
