e677e3d07d86fd0f7a7128e72045134951357546
nrhawkins <nrhawkins@gmail.com>
2015-03-15 19:15:20 -0700

    Update Annotate.scala

--- src/main/scala/experimenting/Annotate.scala
+++ src/main/scala/experimenting/Annotate.scala
@@ -1,8 +1,18 @@
 package experimenting
 
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations
+import edu.stanford.nlp.dcoref.SieveCoreferenceSystem
+import edu.stanford.nlp.dcoref.Document
+import edu.stanford.nlp.dcoref.RuleBasedCorefMentionFinder
+import edu.stanford.nlp.dcoref.Dictionaries
+
 object Annotate {
 
-
-
-
+  def main(Args: Array[String]){
+    println("Hello World!")
+  }
+  
 }
