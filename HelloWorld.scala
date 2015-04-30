1521c179c7554e9a7257c4145ba4a3e64cd10c86
nrhawkins <nrhawkins@gmail.com>
2015-04-29 22:57:44 -0700

    stanford core nlp 3.5.2

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -9,11 +9,35 @@ import edu.stanford.nlp.dcoref.Document
 import edu.stanford.nlp.dcoref.RuleBasedCorefMentionFinder
 import edu.stanford.nlp.dcoref.Dictionaries
 
+import java.util.Properties
+
 object HelloWorld {
 
  def main(Args: Array[String]){
  
    println("Hello World!")
+   
+   val runtime = Runtime.getRuntime()
+
+   val mb = 1024*1024
+   
+   //Print used memory
+   System.out.println("Used Memory:"  + (runtime.totalMemory() - runtime.freeMemory()) / mb)
+ 
+   //Print free memory
+   System.out.println("Free Memory:" + runtime.freeMemory() / mb )
+         
+   //Print total available memory
+   System.out.println("Total Memory:" + runtime.totalMemory() / mb )
+ 
+   //Print Maximum available memory
+   System.out.println("Max Memory:" + runtime.maxMemory() / mb )
+   
+   
+   // creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
+   //val props = new Properties()
+   //props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref")
+   //val pipeline = new StanfordCoreNLP(props)
  
  }
 
--- build.sbt
+++ build.sbt
@@ -1,12 +1,17 @@
 name := "nlp"
+
 version := "1.0"
+
 scalaVersion := "2.10.2"
+
+javaOptions in run += "-Xmx10G"
+
 libraryDependencies ++= Seq(
 "com.github.nscala-time" %% "nscala-time" % "1.4.0",
 "com.typesafe" % "config" % "1.2.1",
 "edu.mit" % "jwi" % "2.2.3",
-"edu.stanford.nlp" % "stanford-corenlp" % "3.5.0",
-"edu.stanford.nlp" % "stanford-parser" % "3.5.0",
+"edu.stanford.nlp" % "stanford-corenlp" % "3.5.1",
+"edu.stanford.nlp" % "stanford-parser" % "3.5.1",
 "edu.washington.cs.knowitall.nlptools" % "nlptools-wordnet-uw_2.10" % "2.4.5",
 "edu.washington.cs.knowitall.taggers" % "taggers-core_2.10" % "0.4",
 "org.scala-lang" % "scala-compiler" % "2.10.2",
